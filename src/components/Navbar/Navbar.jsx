22fc7e8700b9bf307c8864ff3d58dbafcaf61ef0
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-03-06 16:46:55 -0500

    chore: change page title

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,12 @@
 import { Button } from '../Button/Button.jsx'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import styles from './Navbar.module.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export function Navbar() {
     return (
         <div className={styles.navbarContainer}>
-            <h1 className={styles.pageTitle}>Fake Store</h1>
+            <h1 className={styles.pageTitle}>All The Things</h1>
             <div className={styles.buttonContainer}>
                 <Button type={"primary"} label={"Home"}/>
                 <Button type={"primary"} label={"Shop"}/>
