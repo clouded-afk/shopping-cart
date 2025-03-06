914b3e9538c333f2b28cebcec7f2068c904580b5
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-03-06 18:04:32 -0500

    chore: import Navbar and footer components, add container for page content

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { Navbar } from './components/Navbar/Navbar.jsx'
+import { Footer } from './components/Footer/Footer.jsx'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className='mainContainer'>
+      <Navbar />
+      <div className='content'>
+        
+      </div>
+      <Footer />
+    </div>
   )
 }
 
