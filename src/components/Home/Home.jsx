4e9321c02194c198313b6472b2b88fa2da855342
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-03-07 14:48:10 -0500

    chore: setup basic home component to setup/test react-router

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,9 @@
+import styles from './Home.module.css'
+
+export function Home() {
+    return (
+        <div className={styles.homeContainer}>
+            <h1 className={styles.testContent}>THIS IS THE HOME PAGE</h1>
+        </div>
+    )
+}
