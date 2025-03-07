2acd9a20e4d7f958b23a535a3152cdf0388c8088
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-03-07 16:34:18 -0500

    feat: import useNavigate from react router to allow use of the navbar buttons for navigation between pages as a single page application

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,18 @@
+import { useNavigate } from 'react-router-dom'
 import { Button } from '../Button/Button.jsx'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import styles from './Navbar.module.css'
 
 export function Navbar() {
+    const navigate = useNavigate()
     return (
         <div className={styles.navbarContainer}>
             <h1 className={styles.pageTitle}>All The Things</h1>
             <div className={styles.buttonContainer}>
-                <Button type={"primary"} label={"Home"}/>
-                <Button type={"primary"} label={"Shop"}/>
-                <Button type={"secondary"} label={<FontAwesomeIcon className={styles.cartIcon}icon={faShoppingCart}/>}/>
+                <Button type={"primary"} label={"Home"} onClick={() => navigate('/')}/>
+                <Button type={"primary"} label={"Shop"} onClick={() => navigate('/shop')}/>
+                <Button type={"secondary"} label={<FontAwesomeIcon className={styles.cartIcon} icon={faShoppingCart}/>}/>
             </div>
         </div>
     )
