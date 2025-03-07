f4a7a2d1b3613bb85ede5de0db3017ac199a4693
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-03-07 14:48:46 -0500

    chore: setup basic shop component to setup/test react-router

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,9 @@
+import styles from './Shop.module.css'
+
+export function Shop() {
+    return (
+        <div className={styles.shopContainer}>
+            <h1 className={styles.testContent}>THIS IS THE SHOP PAGE</h1>
+        </div>
+    )
+}
