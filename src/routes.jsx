f0d30a6f66792771324aaab95029c2da4afe0831
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-03-07 17:48:48 -0500

    feat: add errorElement to routes array, user will be directed to the error page if the route doesnt exist

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,13 @@
 import { Home } from "./components/Home/Home";
 import { Shop } from "./components/Shop/Shop";
+import { ErrorPage } from "./components/Error/ErrorPage";
 import App from "./App";
 
 const routes = [ 
     {
         path: '/',
         element: <App />,
+        errorElement: <ErrorPage />,
         children: [
             {
                 path: '/',
