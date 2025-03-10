3603114261fa5fb70dec01ee64733ced24fbb81a
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-03-10 18:02:01 -0400

    chore: import fontAEwesome Icons and update labels for each of the quantity buttons

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,4 +1,6 @@
 import { Button } from '../Button/Button.jsx'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
 import styles from "./ItemCard.module.css"
 
 export function ItemCard(
@@ -12,12 +14,12 @@ export function ItemCard(
                 <div className={styles.item}>{itemName}</div>
             </div>
             <div className={styles.buttonContainer}>
-                <Button type={"quantity"} label={"-"}/>
+                <Button type={"quantity"} label={<FontAwesomeIcon icon={faPlus}/>}/>
                 <input 
                     type='number'
                     id='itemQuantity'
                 />
-                <Button type={"quantity"} label={"+"}/>
+                <Button type={"quantity"} label={<FontAwesomeIcon icon={faMinus}/>}/>
             </div>
         </div>
     )
