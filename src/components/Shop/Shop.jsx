24d1896386502201b334703eee10d7c2ae8aba9c
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-03-10 17:52:16 -0400

    chore: import ItemCard component

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,9 +1,11 @@
+import { ItemCard } from '../ItemCard/ItemCard'
 import styles from './Shop.module.css'
 
 export function Shop() {
     return (
         <div className={styles.shopContainer}>
             <h1 className={styles.testContent}>THIS IS THE SHOP PAGE</h1>
+            <ItemCard itemName={"test"}/>
         </div>
     )
 }
