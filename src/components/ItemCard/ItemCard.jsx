c820485f768c3a4401a83ec6b3aa28a9358e41c9
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-03-10 17:53:12 -0400

    chore: add id to input element, change classnames for children of itemContainer

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,4 +1,4 @@
-import Button from '../Button/Button'
+import { Button } from '../Button/Button.jsx'
 import styles from "./ItemCard.module.css"
 
 export function ItemCard(
@@ -8,13 +8,14 @@ export function ItemCard(
     return (
         <div className={styles.container}>
             <div className={styles.itemContainer}>
-                <img className={styles.itemImage} src={itemImage} alt={itemName}/>
-                <div className={styles.itemName}>{itemName}</div>
+                <img className={styles.itemImg} src={itemImage} alt={itemName}/>
+                <div className={styles.item}>{itemName}</div>
             </div>
             <div className={styles.buttonContainer}>
                 <Button type={"quantity"} label={"-"}/>
                 <input 
                     type='number'
+                    id='itemQuantity'
                 />
                 <Button type={"quantity"} label={"+"}/>
             </div>
