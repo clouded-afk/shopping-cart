1bc0828bb1f1c2a7e393611562405d70dc55d4ef
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-03-11 15:33:42 -0400

    feat: add abilty to fetach products from FakeStoreAPI

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,11 +1,23 @@
 import { ItemCard } from '../ItemCard/ItemCard'
+import { useState, useEffect } from 'react'
 import styles from './Shop.module.css'
 
 export function Shop() {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+        .then((response) =>  response.json())
+        .then((data) => setData(data))
+        .catch((error) => console.error(error))
+    }, [])
+
+    console.log(data)
+
     return (
         <div className={styles.shopContainer}>
             <h1 className={styles.testContent}>THIS IS THE SHOP PAGE</h1>
-            <ItemCard itemName={"test"}/>
+            {data.length > 1 && <ItemCard itemName={data[1].title} itemPrice={data[1].price} itemImage={data[1].image}/>}
         </div>
     )
 }
