5fafa71954e625c571aec3ae014cf5c0f350543a
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-03-11 17:29:41 -0400

    feat: add dividers under each header, edit component strucutre

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -17,7 +17,8 @@ export function Shop() {
     return (
         <div className={styles.shopContainer}>
             <div className={styles.categoryContainer}>
-                <h2>Men's Clothing</h2>
+                <h2 className={styles.categoryHeader}>Men's Clothing</h2>
+                <div className={styles.divider}></div>
                 <div className={styles.itemContainer}>
                     {data.slice(0, 4).map((item, index) => (
                         <ItemCard 
@@ -31,7 +32,23 @@ export function Shop() {
                 </div>
             </div>
             <div className={styles.categoryContainer}>
-                <h2>Jewelry</h2>
+                <h2 className={styles.categoryHeader}>Women's Clothing</h2>
+                <div className={styles.divider}></div>
+                <div className={styles.itemContainer}>
+                    {data.slice(14, 20).map((item, index) => (
+                        <ItemCard 
+                            key={index}
+                            itemId={item.id}
+                            itemName={item.title}
+                            itemImage={item.image}
+                            itemPrice={item.price}
+                        />
+                    ))}
+                </div>
+            </div>
+            <div className={styles.categoryContainer}>
+                <h2 className={styles.categoryHeader}>Jewelry</h2>
+                <div className={styles.divider}></div>
                 <div className={styles.itemContainer}>
                     {data.slice(4, 8).map((item, index) => (
                         <ItemCard 
@@ -45,23 +62,10 @@ export function Shop() {
                 </div>
             </div>
             <div className={styles.categoryContainer}>
-                <h2>Electronics</h2>
+                <h2 className={styles.categoryHeader}>Electronics</h2>
+                <div className={styles.divider}></div>
                 <div className={styles.itemContainer}>
                     {data.slice(8, 14).map((item, index) => (
-                        <ItemCard 
-                            key={index}
-                            itemId={item.id}
-                            itemName={item.title}
-                            itemImage={item.image}
-                            itemPrice={item.price}
-                        />
-                    ))}
-                </div>
-            </div>
-            <div className={styles.categoryContainer}>
-                <h2>Women's Clothing</h2>
-                <div className={styles.itemContainer}>
-                    {data.slice(14, 20).map((item, index) => (
                         <ItemCard 
                             key={index}
                             itemId={item.id}
