bfc37190977cc217a046b74f2fd63ee297279074
Josh <160084551+clouded-afk@users.noreply.github.com>
2025-03-11 16:03:57 -0400

    feat: edit component structure to display multiple items from the data array

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -16,8 +16,62 @@ export function Shop() {
 
     return (
         <div className={styles.shopContainer}>
-            <h1 className={styles.testContent}>THIS IS THE SHOP PAGE</h1>
-            {data.length > 1 && <ItemCard itemName={data[1].title} itemPrice={data[1].price} itemImage={data[1].image}/>}
+            <div className={styles.categoryContainer}>
+                <h2>Men's Clothing</h2>
+                <div className={styles.itemContainer}>
+                    {data.slice(0, 4).map((item, index) => (
+                        <ItemCard 
+                            key={index}
+                            itemId={item.id}
+                            itemName={item.title}
+                            itemImage={item.image}
+                            itemPrice={item.price}
+                        />
+                    ))}
+                </div>
+            </div>
+            <div className={styles.categoryContainer}>
+                <h2>Jewelry</h2>
+                <div className={styles.itemContainer}>
+                    {data.slice(4, 8).map((item, index) => (
+                        <ItemCard 
+                            key={index}
+                            itemId={item.id}
+                            itemName={item.title}
+                            itemImage={item.image}
+                            itemPrice={item.price}
+                        />
+                    ))}
+                </div>
+            </div>
+            <div className={styles.categoryContainer}>
+                <h2>Electronics</h2>
+                <div className={styles.itemContainer}>
+                    {data.slice(8, 14).map((item, index) => (
+                        <ItemCard 
+                            key={index}
+                            itemId={item.id}
+                            itemName={item.title}
+                            itemImage={item.image}
+                            itemPrice={item.price}
+                        />
+                    ))}
+                </div>
+            </div>
+            <div className={styles.categoryContainer}>
+                <h2>Women's Clothing</h2>
+                <div className={styles.itemContainer}>
+                    {data.slice(14, 20).map((item, index) => (
+                        <ItemCard 
+                            key={index}
+                            itemId={item.id}
+                            itemName={item.title}
+                            itemImage={item.image}
+                            itemPrice={item.price}
+                        />
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
